302165505ef9c3bd6de63f4b4cc258024b55edd9
jichuuu-00 <jichuuu0417@gmail.com>
2023-02-27 17:10:40 +0900

    중간

--- Quiz/models.py
+++ Quiz/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
 class QuesModel(models.Model):
@@ -25,5 +26,12 @@ class QuesModel(models.Model):
     ans6 = models.CharField(max_length=200,null=True)
     ans7 = models.CharField(max_length=200,null=True)
     # ans8 = models.CharField(max_length=200,null=True)
+    
+class Visitor(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=200,null=True)
+    gender = models.CharField(max_length=200,null=True)
+    date = models.CharField(max_length=200,null=True)
+    score = models.PositiveIntegerField(default=0, null=True)
     
 
